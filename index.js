038ef7b8c34dfcbb6130ebb530b1913ee48135e2
javadance <park.cheongu@gmail.com>
2020-12-19 17:35:15 +0900

    protect param

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://cheongu:Wldud1009!!@cluster0.xngwf.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
+app.get('/', (req, res) => res.send('Hello World!!!!!!^^^^^^^^!!!!!!'))
 
+app.post('/register',(req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
